67757f4f6338a840b1a8751f418f14eecbe878ae
Adam <adamberces@gmail.com>
2023-02-06 20:38:28 +0100

    Shader class improvement

--- include/shader.hpp
+++ include/shader.hpp
@@ -13,41 +13,18 @@ using UID = unsigned int;
 static constexpr size_t INFOLOG_BUFSZ = 0x200;
 
 /////////////////////////////////////////////////////////////////////////////////////////
-// Type safe wrapper for OpenGL shader program type macros
- 
-enum class ShaderType_e 
-{
-    VERTEX,
-    FRAGMENT
-};
-
-template<ShaderType_e ShaderType>
-struct shadertype_trait;
-
-template<>
-struct shadertype_trait<ShaderType_e::VERTEX>
-{
-    static constexpr int GLShaderType = GL_VERTEX_SHADER;
-};
-
-template<>
-struct shadertype_trait<ShaderType_e::FRAGMENT>
-{
-    static constexpr int GLShaderType = GL_FRAGMENT_SHADER;
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////
 // Represents a shader source
  
 class Shader_c
 {
+protected:
     UID Id;
 
     virtual void createShaderObject(ShaderType_e) = 0
 
     void compileShader(std::string FileName, std::string Source)
     {
-        Id = glCreateShader(shadertype_trait<ShaderType>::GLShaderType);
+        createShaderObject();
         glShaderSource(Id, 1, &Source, NULL);
         glCompileShader(Id);
 
@@ -58,7 +35,8 @@ class Shader_c
         if (!success)
         {
             glGetShaderInfoLog(Id, INFOLOG_BUFSZ, NULL, infoLog);
-            std::cout << "Shader compilation failed: " << infoLog << std::endl;
+            std::cout << "Shader file (" << FileName << ") compilation failed: ";
+            std::cout << std::endl; << infoLog << std::endl;
         }
     }
 
@@ -78,5 +56,30 @@ public:
             std::cout << "Loading GLSL file failed: " << FileName << std::endl;
         }
         
+    }
+};
+
+class VertexShader_c : public Shader_c
+{
+public:
+    using Shader_c::Shader_c;
+
+private:
+    void createShaderObject(ShaderType_e) override
+    {
+        Id = glCreateShader(GL_VERTEX_SHADER);
+    }
+};
+
+
+class FragmentShader_c : public Shader_c
+{
+public:
+    using Shader_c::Shader_c;
+
+private:
+    void createShaderObject(ShaderType_e) override
+    {
+        Id = glCreateShader(GL_FRAGMENT_SHADER);
     }
 };
--- include/shaderfactory.hpp
+++ include/shaderfactory.hpp
@@ -1,27 +1,29 @@
 #pragma once
 
-#include <map>
 #include <string>
+#include <memory>
 #include <filesystem>
+#include <unordered_map>
 
 #include "shader.hpp"
 
 
+static const std::string GLSL_PATH = "assets/shaders/";
 static const std::string GLSL_EXTENSION = ".glsl";
 
 class ShaderRepository_c
 {
-
+    std::unordered_map<std::string, std::shared_ptr<Shader_c>> Shaders;
 
 public:
     ShaderFactory_c(std::string Path)
     {
         for (const auto& file :
-             std::filesystem::recursive_directory_iterator)
+             std::filesystem::recursive_directory_iterator(GLSL_PATH))
         {
             if (file.path().extension() == GLSL_EXTENSION)
             {
-                
+
             }
         }
     }
