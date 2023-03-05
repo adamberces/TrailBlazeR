9f51e39bb1fda69bfd72da9669e254da72805954
Adam <adamberces@gmail.com>
2023-03-05 21:03:19 +0100

    Add empty Scene and Ball classes

---
+++ include/game/ball/ball.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include <game/map/map.hpp>
+
+
+namespace trailblazer::gfx
+{
+
+class Ball_c
+{
+
+    trailblazer::gfx::BallPipeline_c ppl;
+
+};
+
+} // namespace trailblazer::gfx
---
+++ include/game/gfx/scene.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include <game/map/map.hpp>
+
+
+namespace trailblazer::gfx
+{
+
+class Scene_c
+{
+
+};
+
+} // namespace trailblazer::gfx
