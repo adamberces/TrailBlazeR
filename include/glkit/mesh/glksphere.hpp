2a618e94622959ee1a1f062e06b28fc2163b0702
Adam <adamberces@gmail.com>
2023-03-05 12:45:56 +0100

    Add Sphere to GLKit Mesh objects

--- include/glkit/mesh/glksphere.hpp
+++ include/glkit/mesh/glksphere.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <utility>
+#include <glkit/mesh/glkmesh.hpp>
+
+
 namespace glkit::mesh
 {
+
+class GLKSphere_c : public GLKMesh_i
+{
+    std::pair<GLKMesh_i::vertex_vector_t, GLKMesh_i::element_vector_t>
+    constructSphereVertexData(unsigned int u, unsigned int v);
+
+public:
+    GLKSphere_c(unsigned int u, unsigned int v);
+};
 
 } // namespace glkit::mesh
--- src/glkit/mesh/glksphere.cpp
+++ src/glkit/mesh/glksphere.cpp
@@ -1,1 +1,74 @@
+#include <cmath>
+#include <iostream>
 #include <glkit/mesh/glksphere.hpp>
+
+
+namespace glkit::mesh
+{
+
+static constexpr float Pi = 3.141592653589;
+
+std::pair<GLKMesh_i::vertex_vector_t, GLKMesh_i::element_vector_t>
+GLKSphere_c::constructSphereVertexData(unsigned int u_max, unsigned int v_max)
+{
+    vertex_vector_t v;
+    element_vector_t e;
+
+    unsigned int ix = 0;
+    float step_u = (Pi * 2) / u_max;
+    float step_v = Pi / v_max;
+
+    for (unsigned int i = 0; i < u_max; i++)
+    {
+        for (unsigned int j = 0; j < v_max; j++)
+        {
+            float u1 = i * step_u;
+            float v1 = j * step_v;
+            float u2 = (i + 1) * step_u;
+            float v2 = (j + 1) * step_v;
+    
+            float x0 = ::cos(u1) * ::sin(v1);
+            float y0 = ::sin(u1) * ::sin(v1);
+            float z0 = ::cos(v1);
+
+            float x1 = ::cos(u1) * ::sin(v2);
+            float y1 = ::sin(u1) * ::sin(v2);
+            float z1 = ::cos(v2);
+
+            float x2 = ::cos(u2) * ::sin(v2);
+            float y2 = ::sin(u2) * ::sin(v2);
+            float z2 = ::cos(v2);
+
+            float x3 = ::cos(u2) * ::sin(v1);
+            float y3 = ::sin(u2) * ::sin(v1);
+            float z3 = ::cos(v1);
+
+            v.insert(v.end(),
+            {
+                x0, y0, z0,
+                x1, y1, z1,
+                x2, y2, z2,
+                x3, y3, z3
+            });
+
+            e.insert(e.end(),
+            {
+                ix, ix + 1, ix + 3,    // triangle 1
+                ix + 1, ix + 2, ix + 3 // triangle 2
+            });
+
+            ix += 4;
+        }
+    }
+
+    return std::make_pair(v, e);
+}
+
+GLKSphere_c::GLKSphere_c(unsigned int u, unsigned int v)
+{
+    auto data = constructSphereVertexData(u, v);
+        
+    initialize(data.first, data.second);
+}
+
+} // namespace glkit::mesh
