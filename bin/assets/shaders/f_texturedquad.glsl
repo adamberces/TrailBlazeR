ad00a47f9e282cccde957f7d8bb4a125add9195d
Adam <adamberces@gmail.com>
2023-03-12 21:00:32 +0100

    Fix FragColor bug

--- bin/assets/shaders/f_texturedquad.glsl
+++ bin/assets/shaders/f_texturedquad.glsl
@@ -1,10 +1,11 @@
 #version 330 core
 
 in vec2 TexCoord;
-out vec4 color;
+out vec4 FragColor;
+
 uniform sampler2D bgTexture;
 
 void main()
 {
-    color = texture(bgTexture, TexCoord);
+    FragColor = texture(bgTexture, TexCoord);
 }
