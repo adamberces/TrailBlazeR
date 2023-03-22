a78f912b6bb063fccc812b8072c07f70a49a26da
Adam <adamberces@gmail.com>
2023-03-22 20:58:13 +0100

    Add uniform functor for orthogonal projection

--- include/glkit/functors/ortho.hpp
+++ include/glkit/functors/ortho.hpp
@@ -9,27 +9,20 @@ namespace glkit::functors
 /////////////////////////////////////////////////////////////////////////////////////////
 // Uniform functor for perspective projection matrix generation
 
-UNIFORM_ARGS(ProjectionConfig_s)
+UNIFORM_ARGS(OrthoConfig_s)
 {
-    float FOV;
-
-    unsigned ScreenWidth;
-    unsigned ScreenHeight;
-
-    float NearPlane;
-    float FarPlane;
+    float Xmin = 0.f;
+    float Xmax;
+    float Ymin = 0.f;
+    float Ymax;
 };
 
 static auto OrthogonalProjection_f =
     [](core::uniforms::uniform_args_ptr_t ptr) -> core::uniforms::uniform_types_t
 {
-    ProjectionConfig_s* cfg = dynamic_cast<ProjectionConfig_s*>(ptr);
+    OrthoConfig_s* cfg = dynamic_cast<OrthoConfig_s*>(ptr);
 
-    glm::mat4 projection = glm::mat4(1.0f);
-    projection = glm::perspective(
-        glm::radians(cfg->FOV),
-        float(cfg->ScreenWidth) / float(cfg->ScreenHeight),
-        cfg->NearPlane, cfg->FarPlane);
+    glm::mat4 projection = glm::ortho(cfg->Xmin, cfg->Xmax, cfg->Ymin, cfg->YMax);
         
     return { projection };
 };
--- include/glkit/functors/perspective.hpp
+++ include/glkit/functors/perspective.hpp
@@ -9,20 +9,27 @@ namespace glkit::functors
 /////////////////////////////////////////////////////////////////////////////////////////
 // Uniform functor for perspective projection matrix generation
 
-UNIFORM_ARGS(OrthoConfig_s)
+UNIFORM_ARGS(ProjectionConfig_s)
 {
-    float Xmin = 0.f;
-    float Xmax;
-    float Ymin = 0.f;
-    float Ymax;
+    float FOV;
+
+    unsigned ScreenWidth;
+    unsigned ScreenHeight;
+
+    float NearPlane;
+    float FarPlane;
 };
 
 static auto PerspectiveProjection_f =
     [](core::uniforms::uniform_args_ptr_t ptr) -> core::uniforms::uniform_types_t
 {
-    OrthoConfig_s* cfg = dynamic_cast<OrthoConfig_s*>(ptr);
+    ProjectionConfig_s* cfg = dynamic_cast<ProjectionConfig_s*>(ptr);
 
-    glm::mat4 projection = glm::ortho(cfg->Xmin, cfg->Xmax, cfg->Ymin, cfg->YMax);
+    glm::mat4 projection = glm::mat4(1.0f);
+    projection = glm::perspective(
+        glm::radians(cfg->FOV),
+        float(cfg->ScreenWidth) / float(cfg->ScreenHeight),
+        cfg->NearPlane, cfg->FarPlane);
         
     return { projection };
 };
