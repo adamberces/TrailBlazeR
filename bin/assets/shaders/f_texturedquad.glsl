a382c849464b02cc3c5a01e5ac657bd530619319
Adam <adamberces@gmail.com>
2023-03-29 17:31:31 +0200

    Add alpha blending to the background texture shader and pipeline for the fade in effect

--- bin/assets/shaders/f_texturedquad.glsl
+++ bin/assets/shaders/f_texturedquad.glsl
@@ -4,8 +4,10 @@ in vec2 TexCoord;
 out vec4 FragColor;
 
 uniform sampler2D bgTexture;
+uniform float alpha;
 
 void main()
 {
-    FragColor = texture(bgTexture, TexCoord);
+    vec4 pixel = texture(bgTexture, TexCoord);
+    FragColor = vec4(pixel.xyz, alpha);
 }
--- include/trailblazer/pipelines/pipeline_bkg.hpp
+++ include/trailblazer/pipelines/pipeline_bkg.hpp
@@ -4,7 +4,9 @@
 
 #include <glkit/core/shaders/shaderprogram.hpp>
 #include <glkit/core/uniforms/uniformiface.hpp>
+#include <glkit/functors/float.hpp>
 
+#include <trailblazer/game/clock.hpp>
 #include <trailblazer/game/constants.hpp>
 
 
@@ -19,17 +21,27 @@ class BackgroundPipeline_c
 {
     std::unique_ptr<glkit::drawables::GLKBackground_c> Background;
     std::unique_ptr<glkit::core::shaders::ShaderProgram_c> ShaderProgram;
+    std::unique_ptr<glkit::core::uniforms::ShaderUniformInterface_c> Uniforms;
 
 public:
+    glkit::functors::Float_s AlphaConfig;
+
     inline void run()
     {
-        if (!Background || !ShaderProgram)
+        if (!Background || !ShaderProgram || !Uniforms)
         {
             throw std::runtime_error("BackgroundPipeline_c::run: call setup first!");
         }
         
+                
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearDepth(1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         Background->activateTexture();
         ShaderProgram->use();
+        Uniforms->update("alpha", &AlphaConfig);
+
         Background->draw();
     }
 
@@ -40,12 +52,17 @@ public:
         SSL.FragmentShaderPath = "./assets/shaders/f_texturedquad.glsl";
 
         ShaderProgram = std::make_unique<glkit::core::shaders::ShaderProgram_c>(SSL);
+        Uniforms = std::make_unique<glkit::core::uniforms::ShaderUniformInterface_c>(ShaderProgram->getId());
+        
+        // This uniform is used to fade in/out the background image
+        Uniforms->add("alpha", glkit::functors::Float_f);
     }
 
     explicit BackgroundPipeline_c(std::string fileName) :
         Background(std::make_unique<glkit::drawables::GLKBackground_c>(fileName))
     {
         setup();
+        AlphaConfig.Value = 1.F;
     }
 };
 
